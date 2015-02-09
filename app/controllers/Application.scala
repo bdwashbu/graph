283ae11516d38e7bd97543086ac2cf0fd79c97e5
bdwashbu <bdwashbu@gmail.com>
2015-02-08 22:51:42 -0600

    ajax

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -7,6 +7,12 @@ import java.util.Arrays
 import scala.collection.mutable.ListBuffer
 import play.api.data._
 import play.api.data.Forms._
+import play.api.{Routes}
+import play.api._
+import play.api.mvc._
+import play.api.data._
+import views.html._
+import models._
 
 object Application extends Controller {
 
@@ -14,90 +20,37 @@ object Application extends Controller {
     Ok(views.html.main("test"))
   }
   
-  val userForm = Form(
-    "dir" -> text
-  )
+  def index = Action { implicit request =>
+    Ok("index Call!")
+  }
+  
+  def ajaxCall = Action { implicit request =>
+    println("jjj")
+    Ok("Ajax Call!")
+  }
+  
+//  def javascriptRoutes() = Action { implicit request =>
+//  Ok(
+//    Routes.javascriptRouter("jsRoutes")(
+//    // Routes
+//    controllers.routes.javascript.Application.xxx,
+//    controllers.routes.javascript.Application.yyy
+//    )
+//    ).as(JAVASCRIPT)
+//  }
   
   def getList(dirName: String) = {
     val dir = new File(dirName)
     println(dir.getAbsolutePath)
     views.html.graph(dirName)
   }
-
-  def getDirs(dir: String): List[(String, String)] = {
-
-    if (dir == null) {
-      Nil
-    } else {
-      var result = ListBuffer[(String, String)]()
-      val newDir = if (dir.charAt(dir.length() - 1) == '\\') {
-        dir.substring(0, dir.length() - 1) + "/";
-      } else if (dir.charAt(dir.length() - 1) != '/') {
-        dir + "/";
-      } else {
-        ""
-      }
-
-      val finalDir = java.net.URLDecoder.decode(newDir, "UTF-8");
-
-      if (new File(finalDir).exists()) {
-        val files = new File(finalDir).list(new FilenameFilter() {
-          override def accept(dir: File, name: String): Boolean = {
-            name.charAt(0) != '.';
-          }
-        })
-        Arrays.sort(files, String.CASE_INSENSITIVE_ORDER);
-        // All dirs
-        for (file <- files) {
-          if (new File(finalDir, file).isDirectory()) {
-            val what = (finalDir, file)
-            result += what
-          }
-        }
-        result.toList
-      } else {
-        Nil
-      }
-    }
-  }
   
-  def getFiles(dir: String): List[(String, String, String)] = {
-
-    if (dir == null) {
-      Nil
-    } else {
-      var result = ListBuffer[(String, String, String)]()
-      val newDir = if (dir.charAt(dir.length() - 1) == '\\') {
-        dir.substring(0, dir.length() - 1) + "/";
-      } else if (dir.charAt(dir.length() - 1) != '/') {
-        dir + "/";
-      } else {
-        ""
-      }
-
-      val finalDir = java.net.URLDecoder.decode(newDir, "UTF-8");
-
-      if (new File(finalDir).exists()) {
-        val files = new File(finalDir).list(new FilenameFilter() {
-          override def accept(dir: File, name: String): Boolean = {
-            name.charAt(0) != '.';
-          }
-        })
-        Arrays.sort(files, String.CASE_INSENSITIVE_ORDER);
-
-        // All files
-        for (file <- files) {
-          if (!new File(finalDir, file).isDirectory()) {
-            val dotIndex = file.lastIndexOf('.');
-            val ext = if (dotIndex > 0) file.substring(dotIndex + 1) else "";
-            val what = (ext, finalDir, file)
-            result += what
-          }
-        }
-        result.toList
-      } else {
-        Nil
-      }
-    }
+  def getAltitudeValue(filepath: String): List[Int] = {
+    import scala.io.Source._
+    
+    val lines = fromFile("C:\\Scala\\Git\\graph\\app\\assets\\mops\\Alamogordo").getLines
+    lines.take(10).foreach(println)
+    List(1,2,3)
   }
+
 }
